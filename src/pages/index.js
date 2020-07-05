758e8c8164c548c0a8168521e7a755cbbaa0d137
Bradley White <bradleywhite@Brads-MacBook-Pro.local>
2020-07-05 10:26:32 -0400

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ class HomeIndex extends React.Component {
 
                     <section id="one">
                         <header className="major">
-                            <h2>Welcome to My Sharing Space</h2>
+                            <h2>Welcome to My Sharing Space that uses Notion to manage contentco-x3family</h2>
                         </header>
                         <h2>There is more to life than my day job. This is my latest project to ensure other distractions</h2>
                     </section>
